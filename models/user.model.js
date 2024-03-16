b0d18043c32cb119363e20bd6f45a9b13ebcf83c
ShobhitPatra <shobhit9999999@gmail.com>
2024-03-16 19:56:33 +0530

    conversation  model

--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female"],
-  },
-  avatar: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
