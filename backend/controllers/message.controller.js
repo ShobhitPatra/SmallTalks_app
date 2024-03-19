88b6a24d53318669ead3c76f8cffd8361b7c85a3
ShobhitPatra <shobhit9999999@gmail.com>
2024-03-19 23:31:58 +0530

    backend tested

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import { promise } from "zod";
 import Conversation from "../../models/conversation.model.js";
 import Message from "../../models/message.model.js";
 
@@ -26,7 +27,12 @@ const sendMessage = async (req, res) => {
 
     if (newMessage) {
       conversation.messages.push(newMessage._id);
+      console.log("mesg pushed");
     }
+
+    // await Promise.all(conversation.save(), newMessage.save());
+    await conversation.save();
+    await newMessage.save();
 
     res.status(201).json({
       newMessage,
--- models/conversation.model.js
+++ models/conversation.model.js
@@ -12,7 +12,6 @@ const conversationSchema = new mongoose.Schema(
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Message",
-        default: [],
       },
     ],
   },
